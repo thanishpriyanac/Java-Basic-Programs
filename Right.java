fa6d8aeb271e52ecc66817d001bc47927e7a43d3
THANISH PRIYAN A C <159042823+thanishpriyanac@users.noreply.github.com>
2024-02-27 18:10:17 +0530

    Add files via upload

---
+++ Right.java
@@ -0,0 +1,15 @@
+public class Right
+{
+public static void main(String args[])
+{
+int a, b;
+for(a=0; a<=5; a++)
+{
+for(b=0; b<=a; b++)
+{   
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
---
+++ Starpat.java
@@ -0,0 +1,15 @@
+public class Starpat
+{
+public static void main (String[] args)
+{
+int a,b;
+for(a=1;a<=5;a++)
+{
+for(b=1;b<=5;b++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}

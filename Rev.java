a07c09ee83c8f8774b1a17bc1e596e2bcd354691
THANISH PRIYAN A C <159042823+thanishpriyanac@users.noreply.github.com>
2024-02-27 18:15:45 +0530

    Add files via upload

---
+++ Rev.java
@@ -0,0 +1,14 @@
+public class Rev  
+{  
+public static void main(String[] args)   
+{  
+int a = 956626, b = 0;  
+while(number != 0)   
+{  
+int rem = number % 10;  
+b = b * 10 + rem;  
+a = a/10;  
+}  
+System.out.println("Reverse of the given number is: " + b);  
+}  
+}
